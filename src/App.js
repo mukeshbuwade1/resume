3cced37646ac8112b148e1e7397a5536bff41af7
mukesh buwade <mukeshbuwade2017@gmail.com>
2022-03-08 23:38:55 +0530

    main component added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route, 
+} from "react-router-dom";
 import './App.css';
+import Header from './components/Header';
+import About from "./pages/About";
+import Resume from './pages/Resume';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router >
+      <Header />
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </Router>
     </div>
   );
 }
